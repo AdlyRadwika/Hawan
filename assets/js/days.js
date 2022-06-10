2f559df7e4b5ce1a8b13fefa4f673764dff28c27
AdlyRadwika <85500151+AdlyRadwika@users.noreply.github.com>
2022-06-10 10:36:02 +0700

    loop fixed but owl-carousel won't load

--- assets/js/days.js
+++ assets/js/days.js
@@ -9,36 +9,40 @@ function getLoc(e){
     fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${name}?unitGroup=metric&include=days%2Ccurrent%2Chours%2Cevents&key=7TZUUUYQV7KQT9QV96MQJ2LPQ&contentType=json`)
     .then((response) => response.json())
     .then((data) => {
-                    
-        document.querySelector("#owl").innerHTML = `
-            <div class="card w-100">
-            <h1 class="card-header">${data.days[1].datetime}</h1>
-                <div class="card-body">
-                    <div class="locInfo">
-                        <div class="conds">
-                            <img src="assets/icons2/${data.days[1].icon}.svg" class="weatherIcon2 mx-auto">
-                            <div class="desc">
-                                <h4>${data.days[1].description}</h4>
-                            </div>
+        
+        for (let i = 1; i < 8; i++) {
+
+            document.querySelector("#days").innerHTML += `
+                <div class="card w-100">
+                    <h1 class="card-header">${data.days[i].datetime}</h1>
+                        <div class="card-body">
+                            <div class="locInfo">
+                                <div class="conds">
+                                    <img src="assets/icons2/${data.days[i].icon}.svg" class="weatherIcon2 mx-auto">
+                                    <div class="desc">
+                                        <h4>${data.days[i].description}</h4>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-4">
+                                        <h5>Max Temp.</h5>
+                                        <p>${data.days[i].tempmax}°C</p>
+                                    </div>
+                                    <div class="col-4">
+                                        <h5>Avg Temp.</h5>
+                                        <p>${data.days[i].temp}°C</p>
+                                    </div>
+                                    <div class="col-4">
+                                        <h5>Min Temp.</h5>
+                                        <p>${data.days[i].tempmin}°C</p>
+                                    </div>    
+                                </div>
+                            </div>  
                         </div>
-                        <div class="row">
-                            <div class="col-4">
-                                <h5>Max Temp.</h5>
-                                <p>${data.days[1].tempmax}°C</p>
-                            </div>
-                            <div class="col-4">
-                                <h5>Avg Temp.</h5>
-                                <p>${data.days[1].temp}°C</p>
-                            </div>
-                            <div class="col-4">
-                                <h5>Min Temp.</h5>
-                                <p>${data.days[1].tempmin}°C</p>
-                            </div>    
-                        </div>
-                    </div>  
-                </div>
-        </div> 
-        `;
+                </div> 
+            `;
+        }
+
     }).catch((err) => {
 
         document.querySelector(".container").innerHTML = `
